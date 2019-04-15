f2276a6381cf848eb5f8854e2402ddd5eb04d263
wilder <debartolocarminedavide@gmail.com>
2019-04-15 12:50:01 +0200

    state change on click

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react"
+import logo from '../logo.svg';
+
+import "./Header.css"
+
+class Header extends React.Component{
+
+        state = {
+            working: false
+        }
+
+        handelClick =(event) => {
+            this.setState({ working: ! this.state.working})
+        }
+        
+        render() {
+        
+        return (
+            <header isWorking={this.state.working} className="header-header">
+                <img src={logo} className={ this.state.working ? 'header-logo-fast' : 'header-logo'} alt="logo" />
+                <p>
+                Generating a React Project for Simpsons Quotes
+                </p>
+                <a
+                className="header-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+                <input className="btn" type="button" onClick={this.handelClick} />
+            </header>
+        )
+    }
+}
+
+export default Header
